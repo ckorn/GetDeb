46d871fb52d3eda40180ac6b6d2beb6b6b088f05
Christoph Korn <christoph.korn@posteo.de>
2016-10-15 17:39:31 +0200

    calibre: 2.70.0-1~getdeb1
      * New upstream version
         new features:
         - title: "Edit Book: Add a tool to download external resources
         (images/stylesheets/etc.) that are not included in the book."
         tickets: [1620058]
         - title: "Make custom columns available in the Alter Tag Browser->Manage
         categories sub menu"
    
         bug fixes:
         - title: "Conversion: Fix incorrect resolution of references to
         resources in HTML files that exist in a folder level above the OPF file.
         This could lead to styles being incorrectly processed in such HTML
         files."
         - title: "Get Books: Update the amazon store plugins for website
         changes"
         - title: "Update ozon.ru metadata download plugin to fix searching for
         books by ISBN"
         - title: "Edit Book: Fix a regression in the previous release that broke
         the preferences dialog for changing color schemes."
         tickets: [1629294]
         - title: "Edit Book: Better error message when user tries to open a
         Check Book item that refers to a file that has been deleted since the
         last time Check Book was run."
         tickets: [1632897]

--- calibre/patches/disable_plugins.py
+++ calibre/patches/disable_plugins.py
@@ -2,11 +2,11 @@ Description: Disable plugin dialog. It uses a totally non-authenticated and non-
 Author: Martin Pitt <mpitt@debian.org>
 Bug-Debian: http://bugs.debian.org/640026
 
-Index: calibre-0.8.29+dfsg/src/calibre/gui2/actions/preferences.py
+Index: calibre-2.70.0/src/calibre/gui2/actions/preferences.py
 ===================================================================
---- calibre-0.8.29+dfsg.orig/src/calibre/gui2/actions/preferences.py	2011-12-16 05:49:14.000000000 +0100
-+++ calibre-0.8.29+dfsg/src/calibre/gui2/actions/preferences.py	2011-12-20 19:29:04.798468930 +0100
-@@ -28,8 +28,6 @@
+--- calibre-2.70.0.orig/src/calibre/gui2/actions/preferences.py	2016-10-14 19:23:19.142942082 +0200
++++ calibre-2.70.0/src/calibre/gui2/actions/preferences.py	2016-10-14 19:23:19.138941954 +0200
+@@ -29,8 +29,6 @@
              pm.addAction(QIcon(I('config.png')), _('Preferences'), self.do_config)
          cm('welcome wizard', _('Run welcome wizard'),
                  icon='wizard.png', triggered=self.gui.run_wizard)
