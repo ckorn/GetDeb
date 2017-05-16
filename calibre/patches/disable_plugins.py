7ddc0d2fff6049f2f9beed86dc6b7d4aa472800a
Christoph Korn <christoph.korn@posteo.de>
2017-05-16 18:10:08 +0200

    calibre: 2.85.1-1~getdeb1
      * New upstream version

--- calibre/patches/disable_plugins.py
+++ calibre/patches/disable_plugins.py
@@ -2,13 +2,13 @@ Description: Disable plugin dialog. It uses a totally non-authenticated and non-
 Author: Martin Pitt <mpitt@debian.org>
 Bug-Debian: http://bugs.debian.org/640026
 
-Index: calibre-2.70.0/src/calibre/gui2/actions/preferences.py
+Index: calibre-2.85.0/src/calibre/gui2/actions/preferences.py
 ===================================================================
---- calibre-2.70.0.orig/src/calibre/gui2/actions/preferences.py	2016-10-14 19:23:19.142942082 +0200
-+++ calibre-2.70.0/src/calibre/gui2/actions/preferences.py	2016-10-14 19:23:19.138941954 +0200
+--- calibre-2.85.0.orig/src/calibre/gui2/actions/preferences.py	2017-05-12 19:27:50.003599928 +0200
++++ calibre-2.85.0/src/calibre/gui2/actions/preferences.py	2017-05-12 19:27:49.999599992 +0200
 @@ -29,8 +29,6 @@
              pm.addAction(QIcon(I('config.png')), _('Preferences'), self.do_config)
-         cm('welcome wizard', _('Run welcome wizard'),
+         cm('welcome wizard', _('Run welcome &wizard'),
                  icon='wizard.png', triggered=self.gui.run_wizard)
 -        cm('plugin updater', _('Get plugins to enhance calibre'),
 -                icon='plugins/plugin_updater.png', triggered=self.get_plugins)
